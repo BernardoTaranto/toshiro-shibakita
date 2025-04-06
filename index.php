0b83ef252ac685e10a81e6d5ad518f7bf8858f63
Bernardo Taranto <bernardotaranto@gmail.com>
2025-04-06 13:39:49 -0300

    Update index.php
    Atualização

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "192.168.0.10";
 $username = "root";
-$password = "Senha123";
+$password = "Password1234";
 $database = "meubanco";
 
 // Criar conexão
@@ -34,7 +34,7 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "INSERT INTO dados (ProdutoID, Nome_Produto, Categoria_Produto, Codigo_Produto) VALUES ('$valor_rand1', '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
 
 
 if ($link->query($query) === TRUE) {
